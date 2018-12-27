122861469ffa318f67f747f32149fc22d7614881
Cedric <cedric.lor@gmail.com>
2018-12-26 22:52:26 -0300

    BUGGED: Added directPinsSwitch sub

--- src/main.cpp
+++ src/main.cpp
@@ -246,7 +246,7 @@ Scheduler userScheduler; // to control your personal task
 void sendMessage() ; // Prototype so PlatformIO doesn't complain
 
 Task taskSendMessage( TASK_SECOND * 1 , TASK_FOREVER, &sendMessage );
-
+// END FROM PAINLESSMESH BASIC
 
 /////////////////////////////////
 // Tasks related to the IR startup
@@ -311,8 +311,14 @@ void cbtLaserOn() {
 }
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Switches relay pins on and off during PIRStartUp
+void directPinsSwitch(const bool targetState) {              // targetState is HIGH or LOW (HIGH to switch off, LOW to switch on)
+  for (short thisPin = highPinsParityDuringStartup; thisPin < PIN_COUNT; thisPin = thisPin + 2) {        // loop around all the structs representing the pins controlling the relays
+    switchOnOffVariables(thisPin, targetState);
+  }
+}
 
-
+// FROM PAINLESSMESH BASIC
 void sendMessage() {
   String msg = "Hello from node ";
   msg += myMesh.getNodeId();
