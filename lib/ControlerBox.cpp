eb3b1543bbbab63bb383ad09ffd616af67c85404
Cedric <cedric.lor@gmail.com>
2019-01-17 19:59:32 -0300

    Renamed variable myMesh to laserControllerMesh in the libs

--- lib/ControlerBox.cpp
+++ lib/ControlerBox.cpp
@@ -13,9 +13,9 @@ ControlerBox::ControlerBox()
 }
 
 void ControlerBox::boxTypeSelfUpdate(ControlerBox *ControlerBoxes, const short I_NODE_NAME, const short BOXES_I_PREFIX) {
-  ControlerBoxes[I_NODE_NAME - BOXES_I_PREFIX].APIP = myMesh.getAPIP();      // store this boxes APIP in the array of boxes pertaining to the mesh
-  ControlerBoxes[I_NODE_NAME - BOXES_I_PREFIX].stationIP = myMesh.getStationIP(); // store this boxes StationIP in the array of boxes pertaining to the mesh
-  ControlerBoxes[I_NODE_NAME - BOXES_I_PREFIX].nodeId = myMesh.getNodeId();  // store this boxes nodeId in the array of boxes pertaining to the mesh
+  ControlerBoxes[I_NODE_NAME - BOXES_I_PREFIX].APIP = laserControllerMesh.getAPIP();      // store this boxes APIP in the array of boxes pertaining to the mesh
+  ControlerBoxes[I_NODE_NAME - BOXES_I_PREFIX].stationIP = laserControllerMesh.getStationIP(); // store this boxes StationIP in the array of boxes pertaining to the mesh
+  ControlerBoxes[I_NODE_NAME - BOXES_I_PREFIX].nodeId = laserControllerMesh.getNodeId();  // store this boxes nodeId in the array of boxes pertaining to the mesh
 }
 
 // void ControlerBox::boxTypeUpdate(ControlerBox *ControlerBoxes, uint32_t iSenderNodeName, uint32_t senderNodeId, JsonObject& root, const short I_NODE_NAME, const short BOXES_I_PREFIX) {
