1e7be92fbc23c1da69bbc7f9e304a8336e26ad06
Cedric <cedric.lor@gmail.com>
2019-01-19 08:53:45 -0300

    Added class myWebServerBase

---
+++ lib/myWebServerBase.cpp
@@ -0,0 +1,13 @@
+/*
+  myWebServerBase.cpp - Library to handle web server controller related functions.
+  Created by Cedric Lor, January 19, 2019.
+*/
+
+#include "Arduino.h"
+#include "myWebServerBase.h"
+
+myWebServerBase::myWebServerBase(/*int pin*/)
+{
+  // pinMode(pin, OUTPUT);
+  // _pin = pin;
+}
---
+++ lib/myWebServerBase.h
@@ -0,0 +1,19 @@
+/*
+  myWebServerBase.h - Library web server controller related functions.
+  Created by Cedric Lor, January 19, 2019.
+*/
+
+#ifndef myWebServerBase_h
+#define myWebServerBase_h
+
+#include "Arduino.h"
+
+class myWebServerBase
+{
+  public:
+    myWebServerBase(/*int pin*/);
+  private:
+    // int _pin;
+};
+
+#endif
