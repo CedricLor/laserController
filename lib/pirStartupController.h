72fb5f84bdde41bb43154066751dde3a619994df
Cedric <cedric.lor@gmail.com>
2019-01-19 08:24:51 -0300

    Privatized all the members that could be privatized in pirStartupController class

--- lib/pirStartupController.h
+++ lib/pirStartupController.h
@@ -14,9 +14,11 @@ class pirStartupController
   public:
     pirStartupController();
 
+    static Task tPirStartUpDelayBlinkLaser;
+
+  private:
     static const short SI_PIR_START_UP_DELAY_ITERATIONS;
     static const long L_PIR_START_UP_DELAY;
-    static Task tPirStartUpDelayBlinkLaser;
 
     static void cbtPirStartUpDelayBlinkLaser();
     static bool onEnablePirStartUpDelayBlinkLaser();
@@ -27,11 +29,8 @@ class pirStartupController
 
     static Task tLaserOff;
     static Task tLaserOn;
-
-  private:
     static void cbtLaserOff();
     static void cbtLaserOn();
-
 };
 
 #endif
