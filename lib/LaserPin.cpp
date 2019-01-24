25b9fe57dea8a261f897de5c450956107087cf55
Cedric <cedric.lor@gmail.com>
2019-01-23 23:30:49 -0300

    Bug partly resolved

--- lib/LaserPin.cpp
+++ lib/LaserPin.cpp
@@ -34,6 +34,7 @@ LaserPin::LaserPin()
 // INITIALIZE LASER PINS
 void LaserPin::physicalInitLaserPin(const short physicalPinNumber)
 {
+  physical_pin_number = physicalPinNumber;
   pinMode(physicalPinNumber, OUTPUT);     // initialization of the pin connected to each of the relay as output
   digitalWrite(physicalPinNumber, HIGH);  // setting default value of the pins at HIGH (relay closed)
 }
