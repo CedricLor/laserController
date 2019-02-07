75ef185606f2e4c6a8ce3b053c5429b8b261e478
Cedric <cedric.lor@gmail.com>
2019-02-07 18:12:45 -0300

    Created new class LAserGroupedUnit from LaserPin

---
+++ lib/LaserGroupedUnit.cpp
@@ -0,0 +1,221 @@
+/*
+  LaserGroupedUnit.cpp - Library to handle the laser grouped units.
+  Created by Cedric Lor, February 7, 2019.
+*/
+
+#include "Arduino.h"
+#include "LaserGroupedUnit.h"
+
+
+bool const LaserGroupedUnit::_default_pin_on_off_state = HIGH;         // by default, the pin starts as HIGH (the relays is off and laser also) TO ANALYSE: THIS IS WHAT MAKES THE CLICK-CLICK AT STARTUP
+bool const LaserGroupedUnit::_default_pin_on_off_target_state = HIGH; // by default, the pin starts as not having received any request to change its state from a function TO ANALYSE: THIS IS WHAT MAKES THIS CLICK-CLICK AT START UP
+bool const LaserGroupedUnit::_default_pin_blinking_state = false;       // by default, pin starts as in a blinking-cycle TO ANALYSE
+bool const LaserGroupedUnit::_default_pin_pir_state_value = LOW;       // by default, the pin is not controlled by the PIR
+const unsigned long LaserGroupedUnit::_max_interval_on = 600000UL;
+
+/* Default constructor: required by the global.cpp
+   Upon initialization of the board, we create an array of LaserPins without which will be later initialized.
+   Upon creating this array, the pins do not receive any parameters, except the default parameters
+*/
+LaserGroupedUnit::LaserGroupedUnit()
+{
+  on_off = _default_pin_on_off_state;
+  on_off_target = _default_pin_on_off_target_state;
+  blinking = _default_pin_blinking_state;
+  previous_time = millis();
+  blinking_interval = pinBlinkingInterval;
+  pir_state = _default_pin_pir_state_value;
+  last_time_on = 0;     // set at 0 at startup
+  last_time_off = millis();    // set at 0 at startup
+  last_interval_on = 0; // set at 0 at startup
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// INITIALIZE LASER PINS
+void LaserGroupedUnit::physicalInitLaserPin(const short physicalPinNumber)
+{
+  physical_pin_number = physicalPinNumber;
+  pinMode(physicalPinNumber, OUTPUT);     // initialization of the pin connected to each of the relay as output
+  digitalWrite(physicalPinNumber, HIGH);  // setting default value of the pins at HIGH (relay closed)
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// SWITCHES
+/* Switches the blinking state of the pin
+   Called from (i) myMesh and (ii) this class (LaserPin)*/
+void LaserGroupedUnit::switchPointerBlinkCycleState(const bool targetBlinkingState) {
+  /* If the targetBlinkingState passed on to the function is LOW,
+     this function sets the blinking property of this LaserPin as true,
+     meaning it is in a blinking cycle.
+     If the targetBlinkingState passed on to the function is LOW,
+     this function sets the blinking property of this LaserPin as false,
+     meaning it is no longer a blinking cycle. */
+  (targetBlinkingState == LOW) ? blinking = true : blinking = false;
+}
+
+/* This function sets the on_off_target property of this LaserPin (and sets the blinking property of this LaserPin accordingly)
+   Called from (i) pirController, (ii) myMesh, (iii) LaserPinsArray and (iii) this class (LaserPin) */
+void LaserGroupedUnit::switchOnOffVariables(const bool targetOnOffState) {
+  // Serial.printf("MANUAL SWITCHES: switchOnOffVariables(const short thisPin, const bool targetState): switching on/off variables for LaserPins[%u] with targetState = %s \n", thisPin, (targetState == 0 ? "on (LOW)" : "off (HIGH)"));
+  switchPointerBlinkCycleState(targetOnOffState);                                                                        // turn the on_off_target state of the struct on or off; the actual pin will be turned on or off in the LASER SAFETY TIMER
+  // THE FOLLOWING BLOCK NEEDS TO BE REDRAFTED: NOT SATISFACTORY
+  // SHOULD BE SET BEFORE CALLING THIS MEMBER FUNCTION
+  // SHOULD BE MORE FLEXIBLE TO WORK WITH FUTURE SEQUENCES
+  // BUT IT SHALL DO THE WORK TO REPLACE ALL THE UPDATE BLOCK IN LASER SAFETY LOOP WHICH WAS EVEN MORE HIDEOUS THAN THIS
+  // if (index_number > paired_with) {                                   // if this pin is not a master
+  //   if (pairing_type == 1) {                                          // if this pin is in alternate mode with its master
+  //     on_off_target = !targetOnOffState;                              // invert the targetOnOffState
+  //     return;
+  //   }
+  // }
+  // on_off_target = targetOnOffState;                                     // default behavior
+  ((index_number > paired_with) && (pairing_type == 1)) ? (on_off_target = !targetOnOffState): (on_off_target = targetOnOffState);
+}
+
+/* This function switches this LaserPin on and off (and sets the pir_state property of this LaserPin to LOW)
+   It is a manual switch in the sense that, by setting the pir_state of the pins to LOW,
+   the pin is no longer reacting to signals sent by the PIR (IR) sensor.
+   Called from (i) myWebServerController and (ii) LaserPinsArray */
+void LaserGroupedUnit::manualSwitchOneRelay(const bool targetOnOffState) {
+  // Serial.printf("MANUAL SWITCHES: manualSwitchOneRelay(const short thisPin, const bool targetState): switching LaserPins[%u] to targetState %s\n", thisPin, (targetState == 0 ? ": on" : ": off"));      // MIGHT CAUSE A BUG!!!
+  switchOnOffVariables(targetOnOffState);
+  pir_state = LOW;
+}
+
+/* PIR SUBJECTION SWITCHES
+   When clicking on the "On" or "Off" button on the webpage in the PIR column,
+   this function subjects one relay to or releases it from the control of the PIR
+   Called from myWebServerController ONLY */
+void LaserGroupedUnit::inclExclOneRelayInPir(const bool targetPirState) {     // state may be HIGH or LOW. HIGH means that the pin will be under the PIR control. LOW releases it from the PIR control.
+  pir_state = targetPirState;                 // set the pin_state variable in HIGH or LOW mode. In HIGH, the pin will be under the control of the PIR and reciprocally.
+  switchOnOffVariables(HIGH);
+}
+
+// Pairs or unpairs two pins together
+// Called from LaserPinsArray
+void LaserGroupedUnit::pairUnpairPin(const short _pinParityWitness, const short pairingType) {
+  if (pairingType == -1) {
+    _unpairPin();
+  } else {
+    _pairPin(_pinParityWitness, pairingType);
+  }
+}
+
+// Pairs this pin to another pin and allows to pair in any type of pairing (twin or cooperative)
+// Called from pairUnpairPin
+void LaserGroupedUnit::_pairPin(const short _pinParityWitness, const short pairingType) {
+  pairing_type = pairingType;
+  if (pairing_type == 1) {
+    _cooperativePairing(_pinParityWitness);
+    return;
+  }
+  if (pairing_type == 0) {
+    _twinPairing(_pinParityWitness);
+    return;
+  }
+}
+
+// Unpair a pin from other pins
+// Called from pairUnpairPin
+void LaserGroupedUnit::_unpairPin() {
+  paired_with = -1;
+  pairing_type = -1;
+}
+
+// Pairs two adjacent pins together (adjacent in the LaserPinsArray)
+// Called from pairUnpairPin
+void LaserGroupedUnit::_cooperativePairing(const short _pinParityWitness)
+{
+  const short thePairedPinIndexNumber = (_pinParityWitness == 0) ? index_number + 1 : index_number - 1;
+  paired_with = thePairedPinIndexNumber;
+}
+
+void LaserGroupedUnit::_twinPairing(const short _pinParityWitness)
+{
+  const short thePairedPinIndexNumber = (index_number < (PIN_COUNT / 2)) ? index_number + (PIN_COUNT / 2) : index_number - (PIN_COUNT / 2);
+  paired_with = thePairedPinIndexNumber;
+}
+
+// Changes the blinking delay of a single pin
+// Called from (i) LaserPinsArray and (ii) myWebServerController
+void LaserGroupedUnit::changeIndividualBlinkingInterval(const unsigned long targetBlinkingInterval) {
+  blinking_interval = targetBlinkingInterval;
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////
+// Blinking in the class
+void LaserGroupedUnit::blinkLaserInBlinkingCycle() {
+  /*
+    Checks:
+    1. if the pin is in blinking
+    2. if so, if the blinking interval of this laser has elapsed
+    If both conditions are fullfilled, switches the pin on/off target variable to the contrary of the current pin on/off
+    TO ANALYSE: this may overwrite other changes that have been requested at other stages
+  */
+  if (blinking == true) {
+    const unsigned long currentTime = millis();
+    if (currentTime - previous_time > blinking_interval) {
+        on_off_target = !on_off;
+    }
+  }
+}
+
+
+////////////////////////////////////////////////////////////////////////////////////////////
+// IO Functions
+////////////////////////////////////////////////////////////////////////////////////////////
+// Execute Updates
+void LaserGroupedUnit::executePinStateChange() {
+  /*
+      Called from within the laser safety loop for each pin
+      Checks whether the current on_off_target state is different than the current on_off state
+      If so:
+      1. turn on or off the laser as requested in the on_off_target_state
+      2. align the on_off state with the on_off_target state
+      3. reset the safety blinking timer of this pin
+      // TO ANALYSE: I have the feeling that the condition to be tested shall be different
+      // in the case a) a laser is in a blinking cycle and in the case b) a laser is not in
+      // a blinking cycle and cooling off
+  */
+  if (on_off != on_off_target) {
+    _markTimeChanges();
+    digitalWrite(physical_pin_number, on_off_target);
+    on_off = on_off_target;
+  }
+}
+
+// Helper function for execute updates
+void LaserGroupedUnit::_markTimeChanges() {
+  const unsigned long currentTime = millis();
+  previous_time = currentTime;
+
+  // If instruction is to turn laserPin on
+  if (on_off_target == LOW) {
+    last_time_off = currentTime;
+    return;
+  }
+  // If instruction is to turn laserPin off
+  last_time_on = currentTime;
+  last_interval_on = last_time_on - last_time_off;
+}
+
+// Laser Protection Switch
+// Function to protect the lasers from staying on over 60 seconds or being turned on again before a 60 seconds delay after having been turned off
+void LaserGroupedUnit::laserProtectionSwitch() {
+  const unsigned long currentTime = millis();
+  // OLD DRAFTING --
+  // if ((digitalRead(physical_pin_number) == LOW) && ((currentTime - last_time_on > _max_interval_on) || (currentTime - last_time_off <  last_interval_on))) {
+  // digitalWrite(physical_pin_number, HIGH);
+  // PROPOSED REDRAFTING --
+  // if ((digitalRead(physical_pin_number) == LOW) && ((currentTime - last_time_off > _max_interval_on) || (currentTime - last_time_on <  last_interval_on))) {
+  // digitalWrite(physical_pin_number, HIGH);
+  if (digitalRead(physical_pin_number) == LOW) {
+    if (currentTime - last_time_off > _max_interval_on) {
+      digitalWrite(physical_pin_number, HIGH);
+      return;
+    }
+    if (currentTime - last_time_on < last_interval_on) {
+      digitalWrite(physical_pin_number, HIGH);
+    }
+  }
+}
---
+++ lib/LaserGroupedUnit.h
@@ -0,0 +1,64 @@
+/*
+  LaserGroupedUnit.h - Library to handle the laser grouped units.
+  Created by Cedric Lor, February 7, 2019.
+*/
+
+#ifndef LaserGroupedUnit_h
+#define LaserGroupedUnit_h
+
+#include "Arduino.h"
+
+class LaserGroupedUnit
+{
+  public:
+    LaserGroupedUnit();
+
+    short physical_pin_number;      // pin physical number of the ESP32 which is controling the relay
+    short index_number;             // LaserPin type index number in the LaserPinsArray
+    bool on_off;                    // variable to store the state (HIGH or LOW) of the pin (LOW = the relay is closed, HIGH = the relay is open)
+    bool on_off_target;             // variable to store the on / off change requests by the various functions
+    bool blinking;                  // is the pin in a blinking cycle (true = the pin is in a blinking cycle, false = the pin is not in a blinking cycle)
+    unsigned long previous_time;    // last time this pin changed state (on or off) // NOTE: redundant with last_time_on and last_time_off below
+    unsigned long blinking_interval;// how long should this pin blink on and off // NOTE: This variable will be replaced by a "sequence" data structure
+    bool pir_state;                 // shall this pin respond to a change coming from the IR sensor; HIGH or LOW: HIGH -> reacting to changes in the PIR
+    short paired_with;              // with which other pin is this pin paired (-1 means it is not paired); the number correspond to the index_number of the paired pin
+    short pairing_type;             // what kind of pairing type does this pin have: -1 is unpaired, 0 is twin pairing (when one is on, the other is also on), 1 is cooperative (when one is on, the other is off and each takes the relay of the other)
+    unsigned long last_time_on;     // last time this pin was turned on
+    unsigned long last_time_off;    // last time this pin was turned off
+    unsigned long last_interval_on; // last interval during which this pin was turned on
+
+    void physicalInitLaserPin(const short physicalPinNumber);
+
+    void switchOnOffVariables(const bool targetState);
+    void switchPointerBlinkCycleState(const bool state);
+
+    void manualSwitchOneRelay(const bool targetState);
+
+    void inclExclOneRelayInPir(const bool state);
+
+    void pairUnpairPin(const short _pinParityWitness, const short pairingType);
+
+    void changeIndividualBlinkingInterval(const unsigned long targetBlinkingInterval);
+
+    void blinkLaserInBlinkingCycle();
+
+    void executePinStateChange();
+    void laserProtectionSwitch();
+
+  private:
+    static bool const _default_pin_on_off_state;         // by default, the pin starts as HIGH (the relays is off and laser also) TO ANALYSE: THIS IS WHAT MAKES THE CLICK-CLICK AT STARTUP
+    static bool const _default_pin_on_off_target_state; // by default, the pin starts as not having received any request to change its state from a function TO ANALYSE: THIS IS WHAT MAKES THIS CLICK-CLICK AT START UP
+    static bool const _default_pin_blinking_state;       // by default, pin starts as in a blinking-cycle TO ANALYSE
+    static bool const _default_pin_pir_state_value;       // by default, the pin is not controlled by the PIR
+
+    static const unsigned long _max_interval_on;
+
+    void _pairPin(const short _pinParityWitness, const short pairingType);
+    void _unpairPin();
+    void _cooperativePairing(const short _pinParityWitness);
+    void _twinPairing(const short _pinParityWitness);
+
+    void _markTimeChanges();
+};
+
+#endif
