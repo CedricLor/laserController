a0cec748aa1cb04f071f24fa6ea55cc93c56e15f
Cedric <cedric.lor@gmail.com>
2019-02-07 19:48:03 -0300

    Created a new class LaserGroupUnitsArray

---
+++ lib/LaserGroupedUnitsArray.cpp
@@ -0,0 +1,12 @@
+/*
+  LaserGroupedUnitsArray_h.cpp - Library to handle the laser grouped units.
+  Created by Cedric Lor, February 7, 2019.
+*/
+
+#include "Arduino.h"
+#include "LaserGroupedUnitsArray.h"
+
+
+LaserGroupedUnitsArray::LaserGroupedUnitsArray()
+{
+}
---
+++ lib/LaserGroupedUnitsArray.h
@@ -0,0 +1,21 @@
+/*
+  LaserGroupedUnitsArray.h - Library to handle the laser grouped units.
+  Created by Cedric Lor, February 7, 2019.
+*/
+
+#ifndef LaserGroupedUnitsArray_h
+#define LaserGroupedUnitsArray_h
+
+#include "Arduino.h"
+
+class LaserGroupedUnitsArray
+{
+  public:
+    LaserGroupedUnitsArray_h();
+
+
+  private:
+
+};
+
+#endif
