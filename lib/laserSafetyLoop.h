13ba80a5769b1d800db4ab20f355c19c1ab6a349
Cedric <cedric.lor@gmail.com>
2019-02-09 10:32:49 -0300

    Deleted 5 unused functions in laser safety loop

--- lib/laserSafetyLoop.h
+++ lib/laserSafetyLoop.h
@@ -15,13 +15,6 @@ class laserSafetyLoop
 
     static void loop();
 
-  private:
-    static void _blinkLaserIfBlinking(LaserPin &LaserPin);
-    static void _blinkLaserIfTimeIsDue(LaserPin &LaserPin);
-
-    static void _ifMasterPairedThenUpdateOnOffOfSlave(const int thisPin);
-    static void _updatePairedSlaveWrapper(const int thisPin);
-    static void _updatePairedSlave(const int thisPin, const bool nextPinOnOffTarget);
 };
 
 #endif
