06808af5746bff9bdf446af0ec539b242616493a
Cedric <cedric.lor@gmail.com>
2019-02-11 17:02:43 -0300

    Comment added to LaserPinsArray

--- lib/LaserPinsArray.cpp
+++ lib/LaserPinsArray.cpp
@@ -26,22 +26,20 @@ void LaserPinsArray::initLaserPins() {
 
 ////////////////////////////////////////////////////////////////////////////////
 // END LOOP
-// One call located in the loop() function of main.cpp
+// Called once, in the loop() function of main.cpp
 void LaserPinsArray::endloop() {
-  // Loop around each struct representing a pin connected to a laser before restarting a global loop and
-  // make any update that may be required. For instance:
-  // - safety time elapsed of lasers in blinking cycle (blinking every 10 to 30 s., to avoid burning the lasers);
-  // - update the paired laser or its pair if the lasers are paired;
-  // and then, execute the updates.
+  
+  /* Loops around each LaserPin representing a pin connected to a relay that
+     controls a laser before restarting a global loop and makes any update that may be required. For instance:
+     - safety time elapsed of lasers in blinking cycle (blinking every 10 to 30 s., to avoid burning the lasers);
+     - update the paired laser or its pair if the lasers are paired;
+     and then, execute the updates. */
   for (short thisPin = 0; thisPin < PIN_COUNT; thisPin++) {
     /* The order of the following function matters:
-    - updateLaserPinValuesFromLaserGroupedUnitOwner is called first as it updates the LaserPin states based on data read into its parent LaserGroupedUnit
-    - setOnOffTarget is called second as it is not an IO function; it only updates the state of the LaserPin state machine;
+    - setOnOffTarget is called first; it is not an IO function; it only updates the states of the LaserPin state machine;
     however, it relies on values updated by updateLaserPinValuesFromLaserGroupedUnitOwner.
     - executePinStateChange is called second as it executes pinChanges (IO function).
-    - laserProtectionSwitch comes last, as it is just an additional check at the end that turns off lasers in case of bug.
-    */
-    // LaserPins[thisPin].updateLaserPinValuesFromLaserGroupedUnitOwner();
+    - laserProtectionSwitch comes last, as it is just an additional check at the end that turns off lasers in case of bug. */
     LaserPins[thisPin].setOnOffTarget();
     LaserPins[thisPin].executePinStateChange();
     LaserPins[thisPin].laserProtectionSwitch();
