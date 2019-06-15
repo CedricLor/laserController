712a45bbf401108cd140a0b35975c1430cc138d2
Cedric Lor <cedric.lor@gmail.com>
2019-06-15 11:35:41 +0200

    Reordered global.cpp to ease its use on several chips

--- lib/global.cpp
+++ lib/global.cpp
@@ -20,16 +20,17 @@ short relayPins[] = { 5, 17, 16, 4 };
 
 const short I_DEFAULT_MASTER_NODE_NAME = 210;
 const short I_NODE_NAME = 201;                          // BOX BY BOX
+const bool MESH_ROOT = true;                            // BOX BY BOX
 // const short I_DEFAULT_MASTER_NODE_NAME = 201;
 // const short I_NODE_NAME = 202;                          // BOX BY BOX
+// const bool MESH_ROOT = false;                            // BOX BY BOX
 const short I_DEFAULT_INTERFACE_NODE_NAME = 001;
-const short IS_INTERFACE = true;                        // BOX BY BOX -- false or true
+const short IS_INTERFACE = false;                        // BOX BY BOX -- false or true
 const short IS_STATION_MANUAL = false;                  // BOX BY BOX
 
 const short I_DEFAULT_SLAVE_ON_OFF_REACTION = 0;
 
 const short BOXES_COUNT = 10;                           // NETWORK BY NETWORK
-const bool MESH_ROOT = true;                            // BOX BY BOX
 
 const short I_MASTER_NODE_PREFIX = 200;                 // Used in MaserSlaveBox class and myWebServerViews to set the name of a new masterBox from website informations (where the boxes are numbered from 1 to 10)
 
