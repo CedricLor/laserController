0f834646efab49cd34c0e2d7992d80c38c94f812
Cedric Lor <cedric.lor@gmail.com>
2019-06-24 11:39:43 +0200

    global.cpp: better definitions of box-specific constants (IS_INTERFACE and IS_STATION_MANUAL for each box); interface box I_NODE_NAME set to 200 (to avoid error in ControlerBox when calculating the index of the ControlerBox in ControlerBoxes[]);  I_DEFAULT_INTERFACE_NODE_NAME set to 200; created a constant I_CONTROLLER_BOX_PREFIX set to 200 (used to calculate MY_INDEX_IN_CB_ARRAY); new constant MY_INDEX_IN_CB_ARRAY to order the boxes properly in the ControlerBoxes[] array

--- lib/global.cpp
+++ lib/global.cpp
@@ -24,15 +24,19 @@ const bool MY_DEBUG = true;
 // const short I_NODE_NAME = 201;                          // BOX BY BOX
 // const short I_DEFAULT_MASTER_NODE_NAME = 210;
 // const bool MESH_ROOT = false;                            // BOX BY BOX
+// const short IS_INTERFACE = false;                        // BOX BY BOX -- false or true
+// const short IS_STATION_MANUAL = false;                  // BOX BY BOX
 
-// CONTROLLER BOX 201
+// CONTROLLER BOX 202
 // const short I_NODE_NAME = 202;                          // BOX BY BOX
 // const short I_DEFAULT_MASTER_NODE_NAME = 201;
 // const bool MESH_ROOT = false;                            // BOX BY BOX
 // Pour Boite 002
+// const short IS_INTERFACE = false;                        // BOX BY BOX -- false or true
+// const short IS_STATION_MANUAL = false;                  // BOX BY BOX
 
-// CONTROLLER BOX 002 - INTERFACE, ROOT NODE AND WEB SERVER
-const short I_NODE_NAME = 002;                          // BOX BY BOX
+// CONTROLLER BOX 200 - INTERFACE, ROOT NODE AND WEB SERVER
+const short I_NODE_NAME = 200;                          // BOX BY BOX
 const short I_DEFAULT_MASTER_NODE_NAME = 003;
 const bool MESH_ROOT = false;                            // BOX BY BOX
 const short IS_INTERFACE = true;                        // BOX BY BOX -- false or true
@@ -40,15 +44,16 @@ const short IS_STATION_MANUAL = true;                  // BOX BY BOX
 
 const uint8_t MESH_CHANNEL = 6;
 
-const short I_DEFAULT_INTERFACE_NODE_NAME = 001;
-const short int MESH_REQUEST_CAPACITY = 7;            // Used for sizing of my custom JSONDocuments received and sent via the mesh and is used in MeshViews and MeshController
+const short I_DEFAULT_INTERFACE_NODE_NAME = 200;
+const short MESH_REQUEST_CAPACITY = 7;            // Used for sizing of my custom JSONDocuments received and sent via the mesh and is used in MeshViews and MeshController
 
 const short I_DEFAULT_SLAVE_ON_OFF_REACTION = 0;
 
 const short BOXES_COUNT = 10;                           // NETWORK BY NETWORK
 
 const short I_MASTER_NODE_PREFIX = 200;                 // Used in MaserSlaveBox class and myWebServerViews to set the name of a new masterBox from website informations (where the boxes are numbered from 1 to 10)
-
+const short I_CONTROLLER_BOX_PREFIX = 200;              // Used to calculate the index of this box in the ControlerBoxes array
+const short MY_INDEX_IN_CB_ARRAY = I_NODE_NAME - I_CONTROLLER_BOX_PREFIX;
 
 short iSlaveOnOffReaction = I_DEFAULT_SLAVE_ON_OFF_REACTION;
 short iMasterNodeName = I_DEFAULT_MASTER_NODE_NAME;
