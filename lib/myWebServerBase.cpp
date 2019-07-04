7a9bf7110318490e2a7bb6c18aa34b7db1f83896
Cedric Lor <cedric.lor@gmail.com>
2019-07-04 10:07:45 +0200

    Serial.prints

--- lib/myWebServerBase.cpp
+++ lib/myWebServerBase.cpp
@@ -371,7 +371,7 @@ void myWebServerBase::_tcbSendWSDataIfChangeBoxState() {
 
     // if the box has an unsignaled change of state
     if (ControlerBoxes[_boxIndex].boxActiveStateHasBeenSignaled == false) {
-      Serial.printf("_tcbSendWSDataIfChangeBoxState::_tcbSendWSDataIfChangeBoxState.State of box [%i] has changed\n", (_boxIndex + B_CONTROLLER_BOX_PREFIX));
+      Serial.printf("_tcbSendWSDataIfChangeBoxState::_tcbSendWSDataIfChangeBoxState. State of box [%i] has changed\n", (_boxIndex + B_CONTROLLER_BOX_PREFIX));
       _messageType = 5;
       _obj["boxState"] = ControlerBoxes[_boxIndex].boxActiveState;
       ControlerBoxes[_boxIndex].boxDeletionHasBeenSignaled = true;
@@ -392,7 +392,7 @@ void myWebServerBase::_tcbSendWSDataIfChangeBoxState() {
 
     if (_messageType != -1) {
       // pass it on, with the type of message (5, 6 or 7) we want to add
-      Serial.printf("_tcbSendWSDataIfChangeBoxState::_tcbSendWSDataIfChangeBoxState. About to call _prepareWSData with message of type %i.\n", _messageType);
+      if (MY_DEBUG) {Serial.printf("_tcbSendWSDataIfChangeBoxState::_tcbSendWSDataIfChangeBoxState. About to call _prepareWSData with message of type %i.\n", _messageType);}
       _prepareWSData(_messageType, _obj);
     }
   }
