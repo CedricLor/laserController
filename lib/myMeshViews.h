e3764ff666978bbe0eb0825f07e2dcb18a3a5368
Cedric Lor <cedric.lor@gmail.com>
2019-07-05 19:13:21 +0200

    myMeshViews: Declared a changeMasterBoxMsg function (non-static)

--- lib/myMeshViews.h
+++ lib/myMeshViews.h
@@ -35,7 +35,7 @@ class myMeshViews
     // static void manualSwitchMsg(const short targetOnOffState);
     // static void inclusionIRMsg(const short targetIrState);
     // static void blinkingIntervalMsg(const unsigned long targetBlinkingInterval);
-    // static void changeMasterBoxMsg(const int newMasterNodeName, const char* newReaction);
+    void changeMasterBoxMsg(const int newMasterNodeName, const char *boxName);
     void statusMsg(uint32_t destNodeId=0);
     void changeBoxTargetState(const char *boxTargetState, const char *boxName);
     // static void pinPairingMsg(const short sTargetPairingType);
