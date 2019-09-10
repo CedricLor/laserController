08b16fb1ee18c08783e8be08a5e6f782f26053aa
Cedric Lor <cedric.lor@gmail.com>
2019-09-10 16:50:02 +0200

    sequence: cleaning up

--- lib/sequence/sequence.cpp
+++ lib/sequence/sequence.cpp
@@ -519,8 +519,6 @@ void sequence::_odtcbPlaySequence(){
 uint32_t const sequence::_ui32SequenceDuration() {
   Serial.println(F("sequence::_ui32SequenceDuration(). Starting."));
   Serial.printf("sequence::_ui32SequenceDuration(). __i16activeSequence = %i\n", _i16ActiveSequence);
-  Serial.printf("sequence::_ui32SequenceDuration(). sequences[_i16ActiveSequence].i16GetBarCountInSequence() = %i\n", sequences[_i16ActiveSequence].i16GetBarCountInSequence());
-  Serial.printf("sequence::_ui32SequenceDuration(). about to return the following value = %u\n", (sequences[_i16ActiveSequence].i16GetBarCountInSequence() * sequences[_i16ActiveSequence]._ui32TheoreticalBarDuration()));
   uint16_t __ui = 0;
   uint32_t __ui32SequenceDuration = 0;
   while (sequences[_i16ActiveSequence].getAssociatedBars()[__ui] != -1) {
