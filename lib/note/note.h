d49e741ff1b1e69b52e383eeef0345c0a068e60a
Cedric Lor <cedric.lor@gmail.com>
2019-09-17 23:22:34 +0200

    nitty gritty

--- lib/note/note.h
+++ lib/note/note.h
@@ -17,8 +17,6 @@ class note
   // TODO: Try to remove friends little by little
   friend class notes;
   friend class bar;
-  friend class bars;
-  friend class sequence;
   friend class test;
   public:
     static tones globalTones;
