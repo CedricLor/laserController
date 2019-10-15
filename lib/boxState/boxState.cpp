a1cda7bd327aef10c9895fa00dd1e8f95bd3d6be
Cedric Lor <cedric.lor@gmail.com>
2019-10-15 17:35:31 +0200

    boxState: starting it in boxState stand alone mode

--- lib/boxState/boxState.cpp
+++ lib/boxState/boxState.cpp
@@ -482,7 +482,7 @@ boxState::boxState(const int16_t _i16Duration,
 /** default constructor */
 boxStateCollection::boxStateCollection(void (*_sendCurrentBoxState)(const int16_t _i16CurrentStateNbr)):
   ui16stepCounter(0),
-  ui16Mode(1),
+  ui16Mode(0),
   sendCurrentBoxState(_sendCurrentBoxState),
   _monitorNoMaster({254}),
   _monitorNoStates({-1}),
