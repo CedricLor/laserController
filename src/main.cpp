5f94606d73576b3fa530d0e89c98993c346854d6
Cedric Lor <cedric.lor@gmail.com>
2019-10-23 00:46:38 +0200

    removed ControlerBoxes definition altogether; thisBox is now defined as a reference to cntrllerBoxesCollection.controllerBoxesArray.at(0)

--- src/main.cpp
+++ src/main.cpp
@@ -28,8 +28,7 @@ extern constexpr short    UI8_BOXES_COUNT                     = 10;
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 controllerBoxesCollection     cntrllerBoxesCollection;
-ControlerBox                  ControlerBoxes[UI8_BOXES_COUNT];
-ControlerBox &thisBox =       ControlerBoxes[0];
+ControlerBox &thisBox =       cntrllerBoxesCollection.controllerBoxesArray.at(0);
 signal                        _signal;
 boxStateCollection            bxStateColl;
 
