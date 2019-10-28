0ce308c9da484db58456f4594fa75e86532eb5aa
Cedric Lor <cedric.lor@gmail.com>
2019-10-28 09:29:28 +0100

    myWSReceiverReconcile: new class to extract clearly code relating to reconciliation between browser data and regular receiver data

---
+++ lib/myWSReceiverReconcile/myWSReceiverReconcile.h
@@ -0,0 +1,43 @@
+/*
+  myWSReceiver.cpp - Library web socket related functions.
+  Created by Cedric Lor, July 9, 2019.
+*/
+
+#ifndef myWSReceiver_h
+#define myWSReceiver_h
+
+#include "Arduino.h"
+#include <mySavedPrefs.h>
+#include <controllerBoxThis.h>
+#include <myWSSender.h>
+#include <myMeshViews.h>
+#include <myMeshStarter.h>
+
+class myWSReceiver
+{
+  public:
+    myWSReceiver(uint8_t *_data);
+
+  private:
+
+    void _actionSwitch(JsonObject& _obj);
+
+    void _onHandshakeCheckWhetherDOMNeedsUpdate(JsonObject& _obj);
+    void _checkConsistancyDOMDB(JsonObject& _joBoxState);
+    void _lookForDisconnectedBoxes(JsonPair& _p);
+    void _checkBoxStateConsistancy(JsonPair& _p);
+    void _lookForDOMMissingRows(JsonObject& _joBoxState);
+
+    void _requestBoxChange(JsonObject& _obj, bool _bBroadcast=false);
+    void _requestIFChange(JsonObject& _obj);
+    
+    void _rebootIF(JsonObject& _obj);
+    void _saveIF(JsonObject& _obj);
+
+    void _savegi8RequestedOTAReboots(JsonObject& _obj);
+    void _requestNetChange(JsonObject& _obj);
+
+};
+#endif
+
+
