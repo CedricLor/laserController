bc27a29ffac07618c9202e1f94f85d082ae0a3ae
Cedric Lor <cedric.lor@gmail.com>
2019-10-29 01:55:09 +0100

    cleaning up

--- lib/myWebServerBase/myWebServerBase.h
+++ lib/myWebServerBase/myWebServerBase.h
@@ -16,14 +16,11 @@ class myWebServerBase
   public:
     myWebServerBase();
 
-    static void myWebServerViews(AsyncWebServerRequest *request, const char* url);
-
   private:
     static AsyncWebServer _asyncServer;
 
     void _onRequest(AsyncWebServerRequest *request);
     void _onBody(AsyncWebServerRequest *request, uint8_t *data, size_t len, size_t index, size_t total);
-    static String _processor(const String& var);
 };
 
 #endif
